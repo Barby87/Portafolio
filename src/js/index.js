ec1185ba31498a93dd18e73cefe16a52e2be8501
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-10-16 02:03:01 -0300

    Agragando página de inicio y arreglando formato

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
+window.onload = () => {
+	setTimeout(function(){
+    const preload = document.getElementById('preload');
+    const page = document.getElementById('page');
+    preload.style.display = 'none';
+    page.style.display = 'block';
+  },5000);
+}
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+// Efecto escritura
+var i = 0;
+var text = "Front-End Developer";
+
+function typing() {
+  if(i<text.length) {
+    document.getElementById('text').innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing,70);
+  }
+}
+
+typing();
+
+
+// Menú lateral
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+// Floating Action Button
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
       
