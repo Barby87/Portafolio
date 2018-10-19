ab26021b7b953191cbcafcc6d31b1a055310b92e
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-10-19 05:34:00 -0300

    Agregando animacines y proyectos

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,52 @@
 window.onload = () => {
-	setTimeout(function(){
+  setTimeout(function () {
     const preload = document.getElementById('preload');
     const page = document.getElementById('page');
     preload.style.display = 'none';
     page.style.display = 'block';
-  },5000);
+  }, 5000);
 }
 
-// Efecto escritura
+// Writing effect
 let i = 0;
 let text = "Front-End Developer...";
 
 function typing() {
-  if(i<text.length) {
+  if (i < text.length) {
     document.getElementById('text').innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing,70);
+    setTimeout(typing, 70);
   }
 }
-
 typing();
 
 
 $(document).ready(function () {
   //Navbar
-  $(window).scroll(function(){
-    if($(window). scrollTop()>300){
-      $('nav').addClass('red');
-    }else{
-      $('nav').removeClass('red');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $('nav').addClass('grey darken-4');
+    } else {
+      $('nav').removeClass('grey darken-4');
     }
   })
   // Floating Action Button
   $('.fixed-action-btn').floatingActionButton();
-  
-  //Tooltip button
+
+  // Tooltip button
   $('.tooltipped').tooltip();
+
+  // Aniview
+  $('.aniview').AniView();
+  //Options
+  var options = {
+    animateThreshold: 100,
+    scrollPollInterval: 20
+  }
+  $('.aniview').AniView(options);
 
 
 });
 
 
-      
+
